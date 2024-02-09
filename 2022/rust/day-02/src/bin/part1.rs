7775c8d37b4d34e1b399cc4fff443cb141d8be19
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-02-09 11:44:35 -0300

    feat: started solving part 1 day 02

--- 2022/rust/day-02/src/bin/part1.rs
+++ 2022/rust/day-02/src/bin/part1.rs
@@ -1,14 +1,46 @@
 // https://adventofcode.com/2022/day/2
-// A rock, B paper, C scissors
-// X rock, Y paper, Z scissors
+// Opponent: A rock, B paper, C scissors
+// Myself:   X rock, Y paper, Z scissors
+//
+
+use std::str::FromStr;
+
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl FromStr for Move {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Move::Rock),
+            "B" | "Y" => Ok(Move::Paper),
+            "C" | "Z" => Ok(Move::Scissors),
+            _ => Err("Incorrect input or move".to_string())
+        }
+
+    }
+}
+
 fn main() {
     let input = include_str!("./input1.txt");
     let output = resolve(input);
     dbg!(output);
 }
 
+
 fn resolve(input: &str) -> String {
-    "15".to_string()
+    let result = input
+        .lines()
+        .map(|line| {
+            let a_match = line
+                .split(" ")
+                .collect::<Vec<&str>>();
+            let match_result = a_match[0].vs(a_match[1]);
+        });
+    "14".to_string()
 }
 
 #[cfg(test)]
